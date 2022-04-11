a5f20f5e2c7f89305d45839644a5d48052c70042
lsmith32 <lmksmith97@gmail.com>
2022-04-11 17:24:30 -0500

    models created and seed index linked

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,16 +8,48 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
+{
+// define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    defaultValue: 10,
+    allowNull: false,
+    validate: {
+      isNumeric: true
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id'
+    }
   }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+}
 );
 
 module.exports = Product;
--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -5,16 +5,25 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+{
+// define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
+  tag_name: {
+    type: DataTypes.STRING,
   }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'tag',
+}
 );
 
 module.exports = Tag;
