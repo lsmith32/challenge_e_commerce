80e2970fdbbed1d6463642b1518587de023e1514
lsmith32 <lmksmith97@gmail.com>
2022-04-12 04:01:54 -0500

    finalized main

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('ecommerce_db', 'root', 'Lasm#0289', {
+const sequelize = new Sequelize('ecommerce_db', 'username', 'password', {
   host: 'localhost',
   dialect: 'mysql'
 });
